24daab5b4abe2c595c5b9fa988250b40f07686f0
Clairon <niwemfurac@gmail.com>
2023-08-08 23:55:14 +0200

    modifying some css

--- scripts/data.js
+++ scripts/data.js
@@ -72,6 +72,150 @@ let restaurants =[
         }
     },
     {
+        name:"Inkasteakhouse",
+        image:"https://res.cloudinary.com/drof5meln/image/upload/v1682074978/inkasteak/InkaSteakhouse_b8yyak.jpg",
+        address:"kiyovu",
+        tel:"07889",
+        descr:"dxdfn nnjn",
+        gallery:[
+            "https://res.cloudinary.com/drof5meln/image/upload/v1681932371/enjoyk/hvn2_dlvbkj.jpg",
+            "https://res.cloudinary.com/drof5meln/image/upload/v1681932371/enjoyk/hvn2_dlvbkj.jpg",
+            "https://res.cloudinary.com/drof5meln/image/upload/v1681932371/enjoyk/hvn2_dlvbkj.jpg"
+        ],
+        location:"7 KN 29 St, Kigali",
+        id:5,
+        menu:{
+            food:[
+                {
+                item:"pizza",
+                price:"8000rwf"
+            },
+            {
+                item:"burger",
+                price:"5000rwf"
+            },
+            {
+                item:"chicken fries",
+                price:"7000rwf"
+            }
+            ],
+            drinks:[
+                {
+                    item:"Milkshake",
+                    price:"3000Rwf"
+                }
+            ]
+        }
+    },
+    {
+        name:"HEAVEN ",
+        image:"https://res.cloudinary.com/drof5meln/image/upload/v1681932371/enjoyk/hvn2_dlvbkj.jpg",
+        address:"kiyovu",
+        tel:"0788978999",
+        descr:"Heaven is a beautiful place with a good view and it has the most delicious food ",
+        gallery:[
+            "https://res.cloudinary.com/drof5meln/image/upload/v1681932371/enjoyk/hvn2_dlvbkj.jpg",
+            "https://res.cloudinary.com/drof5meln/image/upload/v1681932371/enjoyk/hvn2_dlvbkj.jpg",
+            "https://res.cloudinary.com/drof5meln/image/upload/v1681932371/enjoyk/hvn2_dlvbkj.jpg"
+        ],
+        location:"7 KN 29 St, Kigali",
+        id:1,
+        menu:{
+            food:[
+                {
+                item:"pizza",
+                price:"8000rwf"
+            },
+            {
+                item:"burger",
+                price:"5000rwf"
+            },
+            {
+                item:"chicken fries",
+                price:"7000rwf"
+            }
+            ],
+            drinks:[
+                {
+                    item:"Milkshake",
+                    price:"3000Rwf"
+                }
+            ]
+        }
+    },
+    {
+        name:"poivrenoir",
+        image:"https://res.cloudinary.com/drof5meln/image/upload/v1682081103/poivrenoir/poivre-noir-kigali_zetqsx.jpg",
+        address:"kiyovu",
+        tel:"07889",
+        descr:"dxdfn nnjn",
+        gallery:[
+            "https://res.cloudinary.com/drof5meln/image/upload/v1681932371/enjoyk/hvn2_dlvbkj.jpg",
+            "https://res.cloudinary.com/drof5meln/image/upload/v1681932371/enjoyk/hvn2_dlvbkj.jpg",
+            "https://res.cloudinary.com/drof5meln/image/upload/v1681932371/enjoyk/hvn2_dlvbkj.jpg"
+        ],
+        location:"7 KN 29 St, Kigali",
+        id:6,
+        menu:{
+            food:[
+                {
+                item:"pizza",
+                price:"8000rwf"
+            },
+            {
+                item:"burger",
+                price:"5000rwf"
+            },
+            {
+                item:"chicken fries",
+                price:"7000rwf"
+            }
+            ],
+            drinks:[
+                {
+                    item:"Milkshake",
+                    price:"3000Rwf"
+                }
+            ]
+        }
+    },
+    {
+        name:"poivrenoir",
+        image:"https://res.cloudinary.com/drof5meln/image/upload/v1682081103/poivrenoir/poivre-noir-kigali_zetqsx.jpg",
+        address:"kiyovu",
+        tel:"07889",
+        descr:"dxdfn nnjn",
+        gallery:[
+            "https://res.cloudinary.com/drof5meln/image/upload/v1681932371/enjoyk/hvn2_dlvbkj.jpg",
+            "https://res.cloudinary.com/drof5meln/image/upload/v1681932371/enjoyk/hvn2_dlvbkj.jpg",
+            "https://res.cloudinary.com/drof5meln/image/upload/v1681932371/enjoyk/hvn2_dlvbkj.jpg"
+        ],
+        location:"7 KN 29 St, Kigali",
+        id:6,
+        menu:{
+            food:[
+                {
+                item:"pizza",
+                price:"8000rwf"
+            },
+            {
+                item:"burger",
+                price:"5000rwf"
+            },
+            {
+                item:"chicken fries",
+                price:"7000rwf"
+            }
+            ],
+            drinks:[
+                {
+                    item:"Milkshake",
+                    price:"3000Rwf"
+                }
+            ]
+        }
+    },
+    {
         name:"Pilipili",
         image:"https://res.cloudinary.com/drof5meln/image/upload/v1682081824/poivrenoir/pilipili/Pili-Pili-Bistro-Lounge-Bar-Pool-Kigali-36_nwll41.jpg",
         address:"kiyovu",
